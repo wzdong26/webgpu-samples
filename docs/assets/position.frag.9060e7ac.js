3777ecf94af02fe341c90f9e284ceea2bc0a46fc
wzdong <wzdong.26@qq.com>
2024-01-24 18:10:42 +0800

    fix: rebuild

--- docs/assets/position.frag.9060e7ac.js
+++
@@ -1,7 +0,0 @@
-var n=`@fragment\r
-fn main(\r
-    @location(0) fragUV: vec2<f32>,\r
-    @location(1) fragPosition: vec4<f32>\r
-) -> @location(0) vec4<f32> {\r
-    return fragPosition;\r
-}`;export{n as p};
